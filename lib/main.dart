eefa24d65c0e43663eb6977c174164611e140515
mostafaabdelhqq <3b7a299@gmail.com>
2025-03-26 16:11:02 +0200

    update splash screen and add images to assets

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:farmfix/core/utils/app_routes.dart';
 import 'package:farmfix/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,9 +17,16 @@ class FarmFix extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      routerConfig: AppRoutes.router,
+    return ScreenUtilInit(
+      designSize: const Size(430, 932),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp.router(
+          debugShowCheckedModeBanner: false,
+          routerConfig: AppRoutes.router,
+        );
+      },
     );
   }
 }
